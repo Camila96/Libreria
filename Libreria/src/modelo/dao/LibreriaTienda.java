91d50110a5a887493cd2f3511479fadee604f1aa
A2G <angel_3108@hotmail.es>
2015-05-14 21:04:28 -0500

    Creación de la Clase LibreriaTienda

---
+++ Libreria/src/modelo/dao/LibreriaTienda.java
@@ -0,0 +1,39 @@
+package modelo.dao;
+
+import modelo.entidades.Cliente;
+import modelo.entidades.Libro;
+
+public class LibreriaTienda {
+
+	private GestorAutor gestorAutor;
+	private GestorCliente gestorCliente;
+	private GestorLibro gestorLibro;
+	
+	public LibreriaTienda() {
+		
+	}
+	
+	public void resgistrarCompra(Cliente cliente, Libro libro) {
+
+	}
+	
+	public void registrarDevolucion(Cliente cliente, Libro libro) {
+		
+	}
+	
+	public void libroMasVendido(){
+		
+	}
+	
+	public void copiasDisponibles(){
+		
+	}
+	
+	public void copiasvendidas(){
+		
+	}
+	
+	public void regstrarVentas() {
+
+	}
+}
