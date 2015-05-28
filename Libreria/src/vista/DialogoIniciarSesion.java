5009b3e3cf45a65c66d08b51864773b2902b9a3a
SergioNicolas <sergiomol123@gmail.com>
2015-05-28 10:16:09 -0500

    modificaciones iniciar sesion

--- Libreria/src/vista/DialogoIniciarSesion.java
+++ Libreria/src/vista/DialogoIniciarSesion.java
@@ -9,54 +9,57 @@ import javax.swing.JLabel;
 import javax.swing.JPasswordField;
 import javax.swing.JTextField;
 
+import modelos.util.Util;
 import controlador.Controlador;
 
 public class DialogoIniciarSesion extends JDialog{
 
 	private static final long serialVersionUID = 1L;
-	
+
 	private JTextField txtNombre;
 	private JPasswordField txtContrasena;
-	
+
 	public DialogoIniciarSesion(JDialogoPrincipal d , Controlador controlador) {
-		
+
+		setTitle("Iniciar sesion");
 		setSize(300, 200);
+		setIconImage(Util.createImageIcon(ConstantesGUI.I_ICONO_VENTANA).getImage());
 		setLocationRelativeTo(d);
 		setLayout(new GridBagLayout());
-		
+
 		GridBagConstraints gb = new GridBagConstraints();
-				
+
 		JLabel lblNombre = new JLabel("Nombre: ");
 		gb.gridx = 0;
 		gb.gridy = 0;
 		add(lblNombre , gb);
-		
+
 		txtNombre = new JTextField(10);
 		gb.gridx = 1;
 		add(txtNombre);
-		
-		JLabel lblContrasena = new JLabel("Contraseï¿½a: ");
+
+		JLabel lblContraseÃa = new JLabel("Contraseña: ");
 		gb.gridy = 1;
 		gb.gridx = 0;
-		add(lblContrasena , gb);
-	
+		add(lblContraseÃa , gb);
+
 		txtContrasena = new JPasswordField(10);
 		gb.gridx = 1;
 		add(txtContrasena , gb);
-		
+
 		JButton btnCancelar = new JButton("Cancelar");
 		btnCancelar.setActionCommand(Controlador.A_CANCELAR_INCIO_SESION);
 		btnCancelar.addActionListener(controlador);
 		gb.gridx = 0;
 		gb.gridy = 2;
 		add(btnCancelar , gb);
-		
+
 		gb.gridx = 1;
 		JButton btnIniciar = new JButton("Iniciar Sesion");
 		btnIniciar.setActionCommand(Controlador.A_INICIAR_SESION);
 		btnIniciar.addActionListener(controlador);
 		add(btnIniciar , gb);
 	}
-	
+
 
 }
